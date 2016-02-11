3918f4f044e01d9e9c52e9f0ad29a4e8262c6d44
ElKlaus <ELKlsaus>
2016-02-11 22:23:56 +0400

    modal error

--- app/js/main.js
+++ app/js/main.js
@@ -6,8 +6,49 @@
 	};
 
 	var _setUpListners = function  () {
-			// Прослушка событий
+		$('#add-new-item').on('click', _showModal); //Открыть модальное окно
+		$('#add-project').on('submit', _addProject); //Добавление пректа
+		};
+		
+	var _showModal = function (ev) {
+		ev.preventDefault;
+		$('#project-popup').bPopup({
+			speed:650,
+			transition: 'slideDown'
+		});
 	};
+
+	var _addProject = function  (ev) {		
+		console.log('Send to project');
+		ev.preventDefault;
+
+		//Объявляем переменные 
+		var form = $(this),
+			url = 'add_project.php',
+			data = form.serialize();
+		console.log(data);
+
+
+		//Запрос на сервер 
+		/*$.ajax({
+			url: '/path/to/file',
+			type: 'POST',
+			dataType: 'json',
+			data: data
+		})
+		.done(function(ans) {
+			console.log("success");
+			console.log("ans");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});*/
+		
+	}
+
 
 	return {
 		init: init
